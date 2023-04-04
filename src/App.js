311327045c1c49ce72cf7d34e58fb89cd15e0f24
Suelen Santos <suelensantos.cordeiro@gmail.com>
2023-04-04 14:40:27 -0300

    Add bridge files and style for the components

---
+++ src/App.js
@@ -0,0 +1,8 @@
+// According to the new documentation, all functions (components) are represented
+// directly in this App file. I separated them into different component files.
+
+import Game from "./components/Game";
+
+const App = () => <Game />;
+
+export default App;
